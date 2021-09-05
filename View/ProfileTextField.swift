181846ee2f108259232c0c8aa1db4a0f1e82494a
satoutsumugi <tsumugisato0@gmail.com>
2021-09-05 13:20:49 +0900

    ProfileTextField追加

--- View/ProfileTextField.swift
+++ View/ProfileTextField.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+class ProfileTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        self.borderStyle = .roundedRect
+        self.placeholder = placeholder
+        self.backgroundColor = .rgb(red: 245, green: 245, blue: 245)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
